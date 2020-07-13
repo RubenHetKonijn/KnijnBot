e5a4a5e8dab02303609d3a3aae8bc54fddb1c09a
RubenHetKonijn <rubenbeurskens@zoho.com>
2020-07-13 16:44:05 +0200

    commit

--- Brain/main.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-module.exports = (msg) => {
-    axios.get(`http://api.brainshop.ai/get?bid=86315&key=GY6zj8kPTP9rpapx&uid=${msg.member.id}&msg=${msg.content}`).then(res =>{
-        
-    })
-}
